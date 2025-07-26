2ea0c1814571d21181de51eca3f5ff46b9fe4dbe
SVETLANA SUKHOVA <svetsuh24@gmail.com>
2025-07-26 13:13:18 +0300

    JS

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,5 @@
 const STORAGE_KEY = 'feedback-form-state';
 
-// 1. Объект formData с пустыми полями
 let formData = {
   email: '',
   message: '',
@@ -8,16 +7,13 @@ let formData = {
 
 const form = document.querySelector('.feedback-form');
 
-// 3. При загрузке страницы заполняем форму, если есть данные
 populateForm();
 
-// 2. Слушаем input и обновляем formData + localStorage
 form.addEventListener('input', event => {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
-// 4. При submit проверяем поля и очищаем
 form.addEventListener('submit', event => {
   event.preventDefault();
 
@@ -28,13 +24,11 @@ form.addEventListener('submit', event => {
 
   console.log('Form data submitted:', formData);
 
-  // Очистка
   form.reset();
   formData = { email: '', message: '' };
   localStorage.removeItem(STORAGE_KEY);
 });
 
-// Функция заполнения формы из localStorage
 function populateForm() {
   const savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
